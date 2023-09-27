e53de827d5b56187aa69cb1d2409515d663081a9
basil daboor <basildaboor12@gmail.com>
2023-09-27 23:50:47 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 1;
+let num2= 2;
+let multiply = 62;
+let random = Number.isInteger(random) +1;
+let mod = 4;
+let num3 = 3;
+let num4 = 4;
+
+let max = Math.max(1,2,20);
